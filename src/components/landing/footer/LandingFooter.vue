f9506c1935ccf088f6b87ed8d357c28ce185855e
Colin DeCarlo <colin@thedecarlos.ca>
2017-10-25 09:26:50 -0400

    Order by number of contributions

--- src/components/landing/footer/LandingFooter.vue
+++ src/components/landing/footer/LandingFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer-layout flex f-c-c xl-p text-center">
-        <p>A project by <a href="">@colindecarlo</a> <a href="">@bradbrothers</a> <a href="">@darrengalway</a> sponsored by <a href="">@vehikl</a></p>
+        <p>A project by <a href="">@bradbrothers</a> <a href="">@darrengalway</a> <a href="">@colindecarlo</a> sponsored by <a href="">@vehikl</a></p>
     </footer>
 </template>
 
