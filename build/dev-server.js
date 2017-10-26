7f6b0b9597d775f42b9a2dd37f89c1dac9c71433
Brad <brad@bradbrothers.ca>
2017-10-25 20:51:48 -0400

    added production setup to server

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,88 +1,106 @@
-require('./check-versions')()
+require('dotenv').load()
 
-var config = require('../config')
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
-}
+const config = require('../config')
 
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
+const path = require('path')
+const axios = require('axios')
+const express = require('express')
+const webpack = require('webpack')
+const bodyParser = require('body-parser')
+const proxyMiddleware = require('http-proxy-middleware')
+const { check, validationResult } = require('express-validator/check')
+const { matchedData, sanitize } = require('express-validator/filter')
+const webpackConfig = ['testing', 'production'].includes(process.env.NODE_ENV)
   ? require('./webpack.prod.conf')
   : require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+const port = process.env.PORT || config.dev.port
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+const autoOpenBrowser = process.env.NODE_ENV === 'development' && !!config.dev.autoOpenBrowser
 
-var app = express()
-var compiler = webpack(webpackConfig)
-
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
-})
-
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: false,
-  heartbeat: 2000
-})
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
-
-// proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(options.filter || context, options))
-})
-
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
-
-// serve webpack bundle output
-app.use(devMiddleware)
-
-// enable hot-reload and state-preserving
-// compilation error display
-app.use(hotMiddleware)
+const app = express()
+const uri = 'http://localhost:' + port
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+const staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+app.use(staticPath, express.static(config.build.assetsRoot + staticPath))
+// handle fallback for HTML5 history API
+// app.use(require('connect-history-api-fallback')())
+app.use(bodyParser.json())
+app.use(require('helmet')())
 
-var uri = 'http://localhost:' + port
-
-var _resolve
-var readyPromise = new Promise(resolve => {
+let _resolve
+const readyPromise = new Promise(resolve => {
   _resolve = resolve
 })
 
-console.log('> Starting dev server...')
-devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
-  // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
-  _resolve()
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(webpackConfig)
+  // serve pure static assets
+  const devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    quiet: true
+  })
+
+  const hotMiddleware = require('webpack-hot-middleware')(compiler, {
+    log: false,
+    heartbeat: 2000
+  })
+  // force page reload when html-webpack-plugin template changes
+  compiler.plugin('compilation', function (compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+      hotMiddleware.publish({ action: 'reload' })
+      cb()
+    })
+  })
+
+  // serve webpack bundle output
+  app.use(devMiddleware)
+
+  // enable hot-reload and state-preserving
+  // compilation error display
+  app.use(hotMiddleware)
+
+  console.log('> Starting dev server...')
+  devMiddleware.waitUntilValid(() => {
+    console.log('> Listening at ' + uri + '\n')
+    // when env is testing, don't need open it
+    if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
+      require('opn')(uri)
+    }
+    _resolve()
+  })
+}
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/../dist/index.html'))
 })
 
-var server = app.listen(port)
+const validateEmail = check('email').isEmail().withMessage('Sorry, we didn\'t catch that. Try again.').trim()
+app.post('/subscribe', [validateEmail], (request, response) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(422).json({ error: errors.mapped().email.msg });
+  }
+
+  const uri = `${process.env.MAILCHIMP_URI}/lists/${process.env.MAILCHIMP_LIST_ID}/members`
+
+  axios.post(uri, {
+    email_address: request.body.email,
+    status: "subscribed",
+  }, {
+    auth: { user: '', password: process.env.MAILCHIMP_API_KEY },
+  }).then((res) => {
+    return response.send(true)
+  }).catch((error) => {
+    return response.status(422).json({error: error.response.data.detail})
+  })
+})
+
+const server = app.listen(port, () => {
+  console.log(`listening at ${uri}...`)
+})
 
 module.exports = {
   ready: readyPromise,
