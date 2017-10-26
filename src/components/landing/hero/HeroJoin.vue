9a6d5c6ecf21700b331449ac526c9810134f0fc3
Brad <brad@bradbrothers.ca>
2017-10-25 20:52:38 -0400

    mailchimp form integration

--- src/components/landing/hero/HeroJoin.vue
+++ src/components/landing/hero/HeroJoin.vue
@@ -1,17 +1,39 @@
 <template>
-    <form class="hero-form">
+    <div v-if="submitted">
+      <p>Thanks, we'll be in touch!</p>
+    </div>
+    <form v-else @submit.prevent="submit" class="hero-form" novalidate>
         <div class="form-control flex">
-            <input type="text" placeholder="you@example.com" v-model="name">
+            <input id="text" type="email" placeholder="you@example.com" v-model="email">
             <button type="submit" class="button primary">Sign Up</button>
+            <small v-if="error">{{ error }}</small>
         </div>
     </form>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'hero-join',
-    data: () => ({
-        name: null,
-    }),
+    data() {
+        return {
+            email: null,
+            submitted: false,
+            error: null,
+        }
+    },
+    methods: {
+        submit() {
+            console.log(this.email)
+            axios.post('/subscribe', { email: this.email })
+              .then(() => {
+                  this.submitted = true;
+              })
+              .catch((error) => {
+                  this.error = error.response.data.error
+              })
+        },
+    },
 }
 </script>
