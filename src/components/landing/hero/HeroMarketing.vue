b02db1c54cf7ff657e8d5692363124d12bda1c70
Brad <brad@bradbrothers.ca>
2017-10-29 22:40:43 -0400

    update marketing text

--- src/components/landing/hero/HeroMarketing.vue
+++ src/components/landing/hero/HeroMarketing.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="hero-marketing">
         <h1 class="hero-marketing-lead l-m-b">Advanced API analytics and insights</h1>
-        <p class="lead xl-m-b">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, repudiandae minima. Magni dolore quaerat tempora aspernatur recusandae expedita accusamus numquam!</p>
+        <p class="lead xl-m-b">
+            Understanding and monitoring the Key Performance Indicators is the single most important factor in improving your API and delivering the best experience to your clients. Let our detailed analytics give you the knowledge you need to make the best decisions about your system.
+            <br>
+            <strong>Sign up for early access!</strong>
+        </p>
         <hero-join></hero-join>
     </div>
 </template>
