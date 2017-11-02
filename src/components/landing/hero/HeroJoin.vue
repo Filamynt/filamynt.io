02535407023257fea0818606a67bfb3b7f0c7e45
Darren Galway <darrengalwaydesign@gmail.com>
2017-11-02 16:54:47 -0400

    update landing join with new design and states

--- src/components/landing/hero/HeroJoin.vue
+++ src/components/landing/hero/HeroJoin.vue
@@ -1,14 +1,23 @@
 <template>
-    <div v-if="submitted">
-      <p>Thanks, we'll be in touch!</p>
-    </div>
-    <form v-else @submit.prevent="submit" class="hero-form" novalidate>
-        <div class="form-control flex">
-            <input id="text" type="email" placeholder="you@example.com" v-model="email">
-            <button type="submit" class="button primary">Sign Up</button>
-            <small v-if="error">{{ error }}</small>
+    <section class="hero-join card elevation-4 b-radius xl-p">
+        <p class="lead l-m-b text-center">If you're interested in <span class="text-primary text-bold">Filamynt</span> and want to sign up for more information, give us your email and we will provide let you know when it's finished.</p>
+        <form @submit.prevent="submit" class="hero-form" novalidate>
+            <div class="form-control flex">
+                <input id="text" type="email" class="l-g-b" placeholder="you@example.com" v-model="email">
+                <button type="submit" class="button primary">Sign Up</button>
+            </div>
+        </form>
+
+        <div class="well fixed-alert l-m-t bg-success l-p xl-p-l b-radius flex f-s-b f-v-c" v-if="submitted">
+            <p class="text-white">Thanks for joining our newsletter, we'll be in touch!</p>
+            <button class="button medium hallow text-white" @click="submitted = false">Close</button>
         </div>
-    </form>
+
+        <div class="well fixed-alert l-m-t bg-error l-p xl-p-l b-radius flex j-c-s-b flex f-s-b f-v-c" v-if="error && !submitted">
+            <p class="text-white">{{ error }}</p>
+            <button class="button medium hallow text-white" @click="error = false">Close</button>
+        </div>
+    </section>
 </template>
 
 <script>
--- src/components/landing/hero/HeroMarketing.vue
+++ src/components/landing/hero/HeroMarketing.vue
@@ -1,11 +1,7 @@
 <template>
-    <div class="hero-marketing">
+    <div class="hero-marketing relative">
         <h1 class="hero-marketing-lead l-m-b">Advanced API analytics and insights</h1>
-        <p class="lead xl-m-b">
-            Understanding and monitoring the Key Performance Indicators is the single most important factor in improving your API and delivering the best experience to your clients. Let our detailed analytics give you the knowledge you need to make the best decisions about your system.
-            <br>
-            <strong>Sign up for early access!</strong>
-        </p>
+        <p class="lead l-m-b">Understanding and monitoring the Key Performance Indicators is the single most important factor in improving your API and delivering the best experience to your clients.</p>
         <hero-join></hero-join>
     </div>
 </template>
